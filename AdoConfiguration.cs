767f0c57ddbea2ca3a3018681ced54c488b0b550
Alexander Sklar <asklar@microsoft.com>
2020-05-29 15:32:44 -0700

    json config remove non-Blame related files (used only for UpdateADO)

--- AdoConfiguration.cs
+++ AdoConfiguration.cs
@@ -2,40 +2,55 @@ using Microsoft.VisualStudio.Services.Client;
 using Microsoft.VisualStudio.Services.Common;
 using Microsoft.VisualStudio.Services.OAuth;
 using Microsoft.VisualStudio.Services.WebApi;
+using Newtonsoft.Json;
 using System;
-using System.Security.Principal;
+using System.IO;
 
 namespace ADOCLI
 {
-	public static class AdoConfiguration
-	{
-		public static readonly string Uri = "https://microsoft.visualstudio.com/";
+    public class AdoConfiguration
+    {
+        private AdoConfiguration()
+        {
+        }
 
-		public static readonly string Project = "OS";
+        public static AdoConfiguration Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = JsonConvert.DeserializeObject<AdoConfiguration>(File.ReadAllText("adoblame.json"));
+                }
+                return _instance;
+            }
+        }
 
-		public static readonly string RepositoryId = "os";
+        public static AdoConfiguration _instance = null;
+        public string Uri { get; set; }
+        public string Username { get; set; }
+        public string OAuthToken { get; set; }
 
-		public static readonly string AreaPath = "OS\\Core\\DEP\\WinDev\\FxCS";
 
-		private static readonly VssAadCredential _userCredentials = new VssAadCredential(WindowsIdentity.GetCurrent().Name.Split('\\')[1] + "@winse.microsoft.com");
+        private VssCredentials Credentials
+        {
+            get
+            {
+                string token = Environment.GetEnvironmentVariable("OAUTH_TOKEN") ?? OAuthToken;
+                if (!string.IsNullOrEmpty(token))
+                {
+                    return new VssOAuthAccessTokenCredential(token);
+                }
+                else
+                {
+                    return new VssAadCredential(Username);
+                }
+            }
+        }
 
-		private static readonly VssCredentials _labCredentials = (Environment.GetEnvironmentVariable("OAUTH_TOKEN") == null) ? null : new VssOAuthAccessTokenCredential(Environment.GetEnvironmentVariable("OAUTH_TOKEN"));
-
-		private static VssCredentials Credentials
-		{
-			get
-			{
-				if (Environment.GetEnvironmentVariable("OAUTH_TOKEN") != null && _labCredentials != null)
-				{
-					return _labCredentials;
-				}
-				return _userCredentials;
-			}
-		}
-
-		internal static T GetClient<T>() where T : VssHttpClientBase
-		{
-			return new VssConnection(new Uri(Uri), Credentials).GetClient<T>();
-		}
-	}
+        internal static T GetClient<T>() where T : VssHttpClientBase
+        {
+            return new VssConnection(new Uri(Instance.Uri), Instance.Credentials).GetClient<T>();
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ADOBlame
 Find out who changed a tag or field in AzureDevOps workitems
+
+Usage:
+* Edit `adoblame.json`:
+```json
+{
+    "Uri": "https://myCompany.visualstudio.com/",
+    "Username": "myUsername@myCompany.com",
+    "OAuthToken": "optional OAuth token (will use integrated AAD if absent)"
+}
+```
+* `adoblame /id 12345 /tag MyTag`
+
